4f8239192fc716dfb897b1ac18b5b45285e0950c
Haslin96 <115092507+Haslin96@users.noreply.github.com>
2023-02-02 19:27:50 +0300

    Update Homework.sql
    Добавление таблиц со связями многие-ко-многим

--- Homework.sql
+++ Homework.sql
@@ -3,10 +3,22 @@ CREATE TABLE IF NOT exists genre (
 	name VARCHAR(80) not null
 );
 
+CREATE TABLE IF NOT exists genre_executor (
+	genre_id INTEGER references genre(genre_id),
+	executor_id INTEGER references executor(executor_id),
+	constraint pk primary key (genre_id, executor_id)
+);
+
 CREATE TABLE IF NOT exists executor(
 	executor_id SERIAL primary key,
 	name VARCHAR(80) not null,
 	genre_id INTEGER not null references genre(genre_id)
+);
+
+CREATE TABLE IF NOT exists executor_album (
+	executor_id INTEGER references executor(executor_id),
+	album_id INTEGER references album(album_id),
+	constraint pk_1 primary key (executor_id, album_id)
 );
 
 CREATE TABLE IF NOT exists album(
@@ -20,6 +32,12 @@ CREATE TABLE IF NOT exists song(
 	song_id SERIAL primary key references album(album_id),
 	name VARCHAR(80) not null,
 	duration VARCHAR (20) not null
+);
+
+CREATE TABLE IF NOT exists song_compilation (
+	song_id INTEGER references song(song_id),
+	compilation_id INTEGER references compilation(compilation_id),
+	constraint pk_2 primary key (song_id, compilation_id)
 );
 
 CREATE TABLE IF NOT exists compilation(
